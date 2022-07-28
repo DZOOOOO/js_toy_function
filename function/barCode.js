ca48c29a8f445dd506d88ee4996c57634b0e3072
dzoo <dzpro0327@naver.com>
2022-07-28 09:17:38 +0900

    function barCode

--- function/barCode.js
+++ function/barCode.js
@@ -1,20 +1,22 @@
-const large_i = `I`;
-const small_l = `l`;
-let barCodeArray = new Array(10);
+function barCode(length) {
+    const large_i = `I`;
+    const small_l = `l`;
+    let barCodeArray = new Array(length);
 
-for(let i = 0; i < barCodeArray.length; i++) {
-    let random = parseInt(Math.random() * 2);
-    if(random === 1) {
-        barCodeArray[i] = large_i;
-    } else {
-        barCodeArray[i] = small_l;
+    for (let i = 0; i < barCodeArray.length; i++) {
+        let random = parseInt(Math.random() * 2);
+        if (random === 1) {
+            barCodeArray[i] = large_i;
+        } else {
+            barCodeArray[i] = small_l;
+        }
     }
+
+    let barCode = ``;
+
+    for (let i = 0; i < barCodeArray.length; i++) {
+        barCode += barCodeArray[i];
+    }
+
+    return barCode;
 }
-
-let barCode = ``;
-
-for(let i = 0; i < barCodeArray.length; i++) {
-    barCode += barCodeArray[i];
-}
-
-console.log(barCode);
